dccda72f51844a7d35351f1a1d693bcdebc4234c
Davor Bertovic <davor@losinj.com>
2018-03-07 19:29:24 +0000

    Revert "vendor_havoc: use pixel blue color as default"
    This reverts commit f0c930725c317cfca0fa54230e304981957448aa.
    
    Change-Id: I487245c8d0d1b95b12dea8d7ee45dcac2691188e

--- config/version.mk
+++ config/version.mk
@@ -1,57 +1,57 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-Havoc_BRANCH=o
+HAVOC_BRANCH=oreo
 
-# Havoc RELEASE VERSION
-Havoc_VERSION_MAJOR = 13
-Havoc_VERSION_MINOR = 1
-Havoc_VERSION_MAINTENANCE =
+# HAVOC RELEASE VERSION
+HAVOC_VERSION_MAJOR = 13
+HAVOC_VERSION_MINOR = 1
+HAVOC_VERSION_MAINTENANCE =
 
-VERSION := $(Havoc_VERSION_MAJOR).$(Havoc_VERSION_MINOR)$(Havoc_VERSION_MAINTENANCE)
+VERSION := $(HAVOC_VERSION_MAJOR).$(HAVOC_VERSION_MINOR)$(HAVOC_VERSION_MAINTENANCE)
 
-ifndef Havoc_BUILDTYPE
+ifndef HAVOC_BUILDTYPE
     ifdef RELEASE_TYPE
-        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^Havoc_||g')
-        Havoc_BUILDTYPE := $(RELEASE_TYPE)
+        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^HAVOC_||g')
+        HAVOC_BUILDTYPE := $(RELEASE_TYPE)
     else
-        Havoc_BUILDTYPE := UNOFFICIAL
+        HAVOC_BUILDTYPE := UNOFFICIAL
     endif
 endif
 
-ifdef Havoc_BUILDTYPE
-    ifeq ($(Havoc_BUILDTYPE), RELEASE)
-       Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-RELEASE-$(shell date -u +%Y%m%d)
+ifdef HAVOC_BUILDTYPE
+    ifeq ($(HAVOC_BUILDTYPE), RELEASE)
+       HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-RELEASE-$(shell date -u +%Y%m%d)
     endif
-    ifeq ($(Havoc_BUILDTYPE), NIGHTLY)
-        Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
+    ifeq ($(HAVOC_BUILDTYPE), NIGHTLY)
+        HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
     endif
-    ifeq ($(Havoc_BUILDTYPE), WEEKLY)
-        Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-WEEKLY-$(shell date -u +%Y%m%d)
+    ifeq ($(HAVOC_BUILDTYPE), WEEKLY)
+        HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-WEEKLY-$(shell date -u +%Y%m%d)
     endif
-    ifeq ($(Havoc_BUILDTYPE), EXPERIMENTAL)
-        Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
+    ifeq ($(HAVOC_BUILDTYPE), EXPERIMENTAL)
+        HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
     endif
-    ifeq ($(Havoc_BUILDTYPE), UNOFFICIAL)
-        Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+    ifeq ($(HAVOC_BUILDTYPE), UNOFFICIAL)
+        HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
     endif
 else
 #We reset back to UNOFFICIAL
-        Havoc_VERSION := $(TARGET_PRODUCT)_$(Havoc_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+        HAVOC_VERSION := $(TARGET_PRODUCT)_$(HAVOC_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
 endif
 
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(Havoc_VERSION) \
-    ro.havoc.version=$(VERSION)-$(Havoc_BUILDTYPE)
+    ro.modversion=$(HAVOC_VERSION) \
+    ro.havoc.version=$(VERSION)-$(HAVOC_BUILDTYPE)
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.branch=$(Havoc_BRANCH) \
+    ro.havoc.branch=$(HAVOC_BRANCH) \
     ro.romstats.url=http://stats.havoc-rom.com/ \
-    ro.romstats.name=Havoc \
-    ro.romstats.buildtype=$(Havoc_BUILDTYPE) \
+    ro.romstats.name=havoc \
+    ro.romstats.buildtype=$(HAVOC_BUILDTYPE) \
     ro.romstats.version=$(VERSION) \
     ro.romstats.tframe=1 \
     ro.romstats.askfirst=1 \
